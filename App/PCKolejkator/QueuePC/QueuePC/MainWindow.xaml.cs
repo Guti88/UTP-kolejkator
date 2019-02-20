0bb7ec157f12508d47b6e5a94c61a9d0e9e6c28f
Bartłomiej Guzek <bartekguzek@gmail.com>
2019-02-20 19:18:23 +0100

    PC app

--- App/PCKolejkator/QueuePC/QueuePC/MainWindow.xaml.cs
+++ App/PCKolejkator/QueuePC/QueuePC/MainWindow.xaml.cs
@@ -20,9 +20,76 @@ namespace QueuePC
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+        string currentID;
+        string currentQueueStatus;
+        List<String> IDs;
+
+
 		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+        {
+            InitializeComponent();
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            ServiceCommands.GetQueue("1");
+            IDs = ServiceCommands.IDs;
+            currentID = IDs.ElementAt(0);
+            Console.WriteLine(currentID);
+            //Console.WriteLine(currentID);
+            var currentStudent = ServiceCommands.GetStudentDetails<Student>(currentID);
+            lb_Name.Content = currentStudent.name;
+            lb_Surname.Content = currentStudent.surname;
+            lb_Field.Content = currentStudent.field;
+            if (currentStudent.isDaily)
+            {
+                lb_isDaily.Content = "Dzienne";
+            }
+            else
+            {
+                lb_isDaily.Content = "Zaoczne";
+            }
+            if (currentStudent.isMaster)
+            {
+                lb_isMaster.Content = "Magisterskie";
+            }
+            else
+            {
+                lb_isMaster.Content = "Inżynierskie";
+            }
+            listBox.ItemsSource = IDs;
+        }
+
+        private void Btn_pause_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceCommands.EditQueueStatus("1");
+        }
+
+        private void Btn_start_queue_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceCommands.EditQueueStatus("1");
+        }
+
+        private void Btn_stop_queue_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceCommands.EditQueueStatus("1");
+        }
+
+        private void Btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceCommands.DeleteFromQueueByIdAsync(currentID);
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+
+
+        }
+        
+    }
 }
---
+++ App/PCKolejkator/QueuePC/QueuePC/ServiceCommands.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace QueuePC
+{
+    class ServiceCommands
+    {
+        static public List<String> IDs;
+        public static async Task DeleteFromQueueByIdAsync(string _id)  
+        {
+            string myJson = "{\"IndexNumber\":\"" + _id + "\"}";
+            Console.WriteLine(myJson);
+            using (var client = new HttpClient())
+            {
+                var response = await client.DeleteAsync(
+                    "http://utpkolejka.azurewebsites.net/api/student/1",
+                     new StringContent(myJson, Encoding.UTF8, "application/json"));
+                Console.WriteLine(response);
+            }
+
+            /*//{"IndexNumber": "104001"}
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://utpkolejka.azurewebsites.net/api/student/1");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            //httpWebRequest.GetRequestStream();
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                //string json = "{\"IndexNumber\": \"104001\"}";
+                string json = "{\"IndexNumber\":\""+ _id +"\"}";
+                Console.WriteLine(json);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }*/
+        }
+
+        public static void GetQueueStatus()
+        {
+
+        }
+
+        public static void EditQueueStatus(string _id)
+        {
+
+        }
+
+        public static void GetQueue(string _id)
+        {
+            using (var webClient = new WebClient())
+            {
+                var jsonData = string.Empty;
+                try
+                {
+                    jsonData = webClient.DownloadString("http://utpkolejka.azurewebsites.net/api/queue/1");
+                }
+                catch (Exception)
+                {
+                }
+                IDs = JsonConvert.DeserializeObject<List<String>>(jsonData);
+                //Console.WriteLine(IDs.Count);
+            }
+        }
+
+        public static T GetStudentDetails<T>(string _id) where T : new()
+        {
+            using (var webClient = new WebClient())
+            {
+                var jsonData = string.Empty;
+                try
+                {
+                    jsonData = webClient.DownloadString("http://utpkolejka.azurewebsites.net/api/student/" + _id);
+                }
+                catch (Exception) { }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(jsonData) ? JsonConvert.DeserializeObject<T>(jsonData.Substring(1, jsonData.Length - 2)) : new T();
+            }
+        }
+
+    }
+}
